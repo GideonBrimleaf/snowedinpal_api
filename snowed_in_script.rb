2349f0c84afdfa62654b9ffddd1e758b4894e988
Gideon Brimleaf <chrisdryden1@gmail.com>
2019-12-13 22:27:40 +0000

    makes an in-line class for snow

--- snowed_in_script.rb
+++ snowed_in_script.rb
@@ -21,12 +21,45 @@ get '/' do
 end
 
 get '/snowed_in' do
-  forecast = get_forecast
-  @weather_response = am_i_response(forecast['summary'],'heavy_snow')
-  @weather_report_day1 = weather_report(forecast)[0]
-  @weather_report_day2 = weather_report(forecast)[1]
-  @weather_report_day3 = weather_report(forecast)[2]
+  weather_report = WeatherReport.new
+  @weather_answer = weather_report.answer
+  @weather_data = weather_report.data
+
   erb :snow
+end
+
+class WeatherReport
+
+  def initialize
+    @forecast = get_forecast
+  end
+
+  def answer
+    am_i_response(@forecast['summary'],'heavy_snow')
+  end
+
+  def data
+    [
+      weather_report(@forecast)[0], 
+      weather_report(@forecast)[1], 
+      weather_report(@forecast)[2]
+    ]
+  end
+
+  def get_forecast
+    url = 'https://peaceful-scrubland-76997.herokuapp.com/forecasts/three_day'
+    HTTParty.get(url).parsed_response
+  end
+  
+  def am_i_response(weather_summary, weather_type)
+    weather_duration = weather_type == 'heavy_snow' ? weather_summary['heavy_snow_days'] : weather_summary['high_wind_nights']
+    weather_summary[weather_type] == true ? "Aye pal - fae the next #{weather_duration[0]} #{'day'.pluralize(weather_duration[0])}" : "Naw mate, yer alright"
+  end
+  
+  def weather_report(forecast_range)
+    forecast_range['breakdown'].map {|weather_item| weather_item.values}
+  end
+
 end
 
 get '/heavy_wind' do
