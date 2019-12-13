fad7bf8332656e90fcd673dab219c56f75ed5008
Chris Dryden <chrisdryden2@gmail.com>
2019-12-13 14:57:58 +0000

    Tables created with valid info

--- snowed_in_script.rb
+++ snowed_in_script.rb
@@ -9,7 +9,11 @@ end
 
 def am_i_response(weather_summary, weather_type)
   weather_duration = weather_type == 'heavy_snow' ? weather_summary['heavy_snow_days'] : weather_summary['high_wind_nights']
-  weather_summary[weather_type] == true ? "aye pal - fae the next #{weather_duration[0]} #{'day'.pluralize(weather_duration[0])}" : "naw mate, yer alright"
+  weather_summary[weather_type] == true ? "Aye pal - fae the next #{weather_duration[0]} #{'day'.pluralize(weather_duration[0])}" : "Naw mate, yer alright"
+end
+
+def weather_report(forecast_range)
+  forecast_range['breakdown'].map {|weather_item| weather_item.values}
 end
 
 get '/' do
@@ -17,9 +21,15 @@ get '/' do
 end
 
 get '/snowed_in' do
+  @weather_report_day1 = weather_report(get_forecast)[0]
+  @weather_report_day2 = weather_report(get_forecast)[1]
+  @weather_report_day3 = weather_report(get_forecast)[2]
   erb :snow
 end
 
 get '/heavy_wind' do
+  @weather_report_day1 = weather_report(get_forecast)[0]
+  @weather_report_day2 = weather_report(get_forecast)[1]
+  @weather_report_day3 = weather_report(get_forecast)[2]
   erb :wind
 end
