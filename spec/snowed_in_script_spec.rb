6b3dd339f5b482451b8094a29b2cd35c94dbf810
Chris Dryden <chrisdryden2@gmail.com>
2019-12-13 16:32:49 +0000

    basic testing around weather response

--- spec/snowed_in_script_spec.rb
+++ spec/snowed_in_script_spec.rb
@@ -3,7 +3,24 @@
 require_relative '../snowed_in_script.rb'
 
 describe 'am_i_response' do
-   it 'days naw when there is no snow' do
-     weather = {"summary"=>{"heavy_snow"=>false,"high_wind_night"=>false,"cold_night"=>true,"heavy_snow_days"=>[],"high_wind_nights"=>[],"cold_nights"=>[null,"2019-12-14","2019-12-15"]}}
+  it 'says naw when there is nae snow' do
+    weather = {"heavy_snow"=>false, "high_wind_night"=>true, "cold_night"=>true, "heavy_snow_days"=>[], "high_wind_nights"=>[1], "cold_nights"=>[nil, "2019-12-14", "2019-12-15"]}
+    expect(am_i_response(weather, "heavy_snow")).to eql("Naw mate, yer alright")
   end
+  
+  it 'says aye when there is snow' do
+    weather = {"heavy_snow"=>true, "high_wind_night"=>false, "cold_night"=>true, "heavy_snow_days"=>[2], "high_wind_nights"=>[], "cold_nights"=>[nil, "2019-12-14", "2019-12-15"]}
+    expect(am_i_response(weather, "heavy_snow")).to eql("Aye pal - fae the next 2 days")
+  end
+  
+  it 'says naw when there is nae wind' do
+    weather = {"heavy_snow"=>true, "high_wind_night"=>false, "cold_night"=>true, "heavy_snow_days"=>[2], "high_wind_nights"=>[], "cold_nights"=>[nil, "2019-12-14", "2019-12-15"]}
+    expect(am_i_response(weather, "high_wind_night")).to eql("Naw mate, yer alright")
+  end
+  
+  it 'says aye when there is wind' do
+    weather = {"heavy_snow"=>false, "high_wind_night"=>true, "cold_night"=>true, "heavy_snow_days"=>[], "high_wind_nights"=>[1], "cold_nights"=>[nil, "2019-12-14", "2019-12-15"]}
+    expect(am_i_response(weather, "high_wind_night")).to eql("Aye pal - fae the next 1 day")
+  end
+  
 end
